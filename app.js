b78c196211fc4882354f7db05d254e41aae02ef9
Adam <mail@adamkhafagy.com>
2023-02-03 13:56:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const { Octokit } = require('@octokit/rest');
 const cron = require('node-cron');
 const nodemailer = require('nodemailer');
 
-const fromEmail = process.env.FROM_EMAIL;
-const toEmail = process.env.TO_EMAIL;
-const ccEmail = process.env.CC_EMAIL;
-
 const transporter = nodemailer.createTransport({
 	host: 'smtp-mail.outlook.com',
 	secureConnection: false, // TLS requires secureConnection to be false
@@ -17,28 +13,25 @@ const transporter = nodemailer.createTransport({
 		ciphers: 'SSLv3',
 	},
 	auth: {
-		user: fromEmail,
+		user: process.env.FROM_EMAIL,
 		pass: process.env.FROM_PASSWORD,
 	},
 });
 
 const mailNoCommitOptions = {
-	from: fromEmail,
-	cc: ccEmail,
-	to: toEmail,
+	from: process.env.FROM_EMAIL,
+	cc: process.env.CC_EMAIL,
+	to: process.env.TO_EMAIL,
 	subject: 'Git Repo Status Alert',
 	text: 'Hello! We are writing to inform you that you have not submitted a Git commit to the Learning and Projects repository in the last 24 hours. Do better.',
 };
 
 const mailCommitOptions = {
-	from: fromEmail,
-	to: ccEmail,
+	from: process.env.FROM_EMAIL,
+	to: process.env.CC_EMAIL,
 	subject: 'Git Repo Status Alert',
 	text: 'Hello! We are writing to inform you that a Git commit has been submitted to the Learning and Projects repository in the last 24 hours. Keep up the great work!!',
 };
-
-var commitDate = '';
-var currentLocaleDate = '';
 
 const octokit = new Octokit({
 	auth: process.env.GH_KEY,
@@ -49,10 +42,10 @@ async function getCommitsByDay() {
 		repo: process.env.GH_REPO,
 		owner: process.env.GH_AUTHOR,
 	});
-	currentLocaleDate = new Date()
+	var currentLocaleDate = new Date()
 		.toLocaleString('sv', { timeZone: 'America/Toronto' })
 		.slice(0, 10);
-	commitDate = data[0].commit.author.date.slice(0, 10);
+	var commitDate = data[0].commit.author.date.slice(0, 10);
 	if (commitDate === currentLocaleDate) {
 		transporter.sendMail(mailCommitOptions, (error, info) => {
 			if (error) {
@@ -72,12 +65,10 @@ async function getCommitsByDay() {
 	}
 }
 
-cron.schedule(
-	'59 23 * * 1-5 ',
-	() => {
-		getCommitsByDay();
-	},
-	{
+cron.schedule('59 23 * * 1-5 ', () => {
+	getCommitsByDay();
+},
+	      {
 		scheduled: true,
 		timezone: 'America/Toronto',
 	}
